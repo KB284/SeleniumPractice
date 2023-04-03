c7eeb675acd5794a9a680941690dd26beed16d0d
KB284 <kfarah5030@eagle.fgcu.edu>
2023-04-03 09:40:36 -0400

    updated to include different sleep time.

--- src/test/java/com/example/selenpractice/sleep.java
+++ src/test/java/com/example/selenpractice/sleep.java
@@ -2,14 +2,47 @@ package com.example.selenpractice;
 
 import java.lang.Thread;
 
-public class sleep{
-         public static void sleeperAgent(){
-             try{
-                 Thread.sleep(3000);
-             }catch(InterruptedException e){
-                 throw new RuntimeException(e);
-             }
-         }
+public class sleep {
+
+    public static void sleeperAgent5() {
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void sleeperAgent4() {
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void sleeperAgent3() {
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void sleeperAgent2() {
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void sleeperAgent1() {
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
 
 }
