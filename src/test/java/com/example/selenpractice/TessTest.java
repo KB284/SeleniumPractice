907ccf2c348b2e7a18fec68313c5a45dfb97b249
KB284 <kfarah5030@eagle.fgcu.edu>
2023-04-13 11:09:48 -0400

    TIC ID commented out, causing errors.

--- src/test/java/com/example/selenpractice/TessTest.java
+++ src/test/java/com/example/selenpractice/TessTest.java
@@ -1,117 +1,151 @@
 package com.example.selenpractice;
 
-import java.time.Duration;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
 
 public class TessTest {
-    public static void main(String[] args) throws Exception {
-        System.setProperty("webdriver.chrome.driver", "C:\\Users\\khuba\\.cache\\selenium\\chromedriver_win\\chromedriver.exe");
+    private WebDriver driver;
+
+    @BeforeTest
+    public void setup() {
+        // Set the system property to use ChromeDriver
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\khuba\\.cache\\selenium\\chromedriver_win32\\chromedriver.exe");
+
+        // Initialize a new instance of ChromeDriver
+        driver = new ChromeDriver();
 
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--remote-allow-origins=*");
 
         // Create a new instance of the ChromeDriver
-        WebDriver driver = new ChromeDriver(options);
+        driver = new ChromeDriver(options);
 
+        // Navigate to a website
         driver.get("http://127.0.0.1:5000");
 
-        SkyView(driver);
-        RaDecSearch(driver);
-        ObjNameSearch(driver);
-        TicIDSearch(driver);
-        CSV_upload(driver);
+        driver.manage().window().maximize();
+
+        String pageTitle = driver.getTitle();
+        Assert.assertNotNull(pageTitle);
+        Assert.assertNotEquals(pageTitle, "");
+
+        System.out.println(pageTitle);
+
     }
 
-    public static void SkyView(WebDriver driver) {
-        // find the search input field and enter the name of the star
-        WebElement searchInput = driver.findElement(By.id("target"));
-        searchInput.sendKeys("Sirius");
+    @Test(priority = 1)
+    public void SkyView() {
+        //call the webpage loader method
         sleep.sleeperAgent3();
 
+        // Find the search input field and enter the name of the star
+        WebElement searchBox = driver.findElement(By.cssSelector("#target"));
+        searchBox.sendKeys("Sirius");
+        Assert.assertEquals(searchBox.getAttribute("value"), "Sirius");
+        sleep.sleeperAgent3();
+
+    }
+
+    @Test(priority = 2)
+    public void SkyViewButton(){
         WebElement skySubmit = driver.findElement(By.cssSelector("#form-sky > button"));
         skySubmit.click();
+        sleep.sleeperAgent3();
 
         WebElement zoomout = driver.findElement(By.className("zoomMinus"));
         zoomout.click();
+        sleep.sleeperAgent5();
+    }
+
+    @Test(priority = 3)
+    public void RaDecSearch() {
+        WebElement RaDec = driver.findElement(By.xpath("//*[@id=\"search_input\"]"));
+        RaDec.sendKeys("92.0096, -30.5839");
+        Assert.assertEquals(RaDec.getAttribute("value"), "92.0096, -30.5839");
+        sleep.sleeperAgent3();
+
+        WebElement radius = driver.findElement(By.cssSelector("#radius"));
+        radius.sendKeys("1");
+        Assert.assertEquals(radius.getAttribute("value"), "1");
         sleep.sleeperAgent3();
     }
 
-    public static void RaDecSearch(WebDriver driver) {
-        WebElement RaDec = driver.findElement(By.id("search_input"));
-        RaDec.sendKeys("92.0096, -30.5839");
-        sleep.sleeperAgent1();
+    @Test(priority = 4)
+    public void RaDecButton(){
+        WebElement searchButton = driver.findElement(By.cssSelector("body > div > div.query-container > div.search-input > div > form > input[type=submit]:nth-child(4)"));
+        searchButton.click();
+        sleep.sleeperAgent5();
+    }
+
+    @Test(priority = 5)
+    public void ObjNameSearch(){
+        WebElement ObjName = driver.findElement(By.xpath("//*[@id=\"search_input\"]"));
+        ObjName.sendKeys("Sirius");
+        Assert.assertEquals(ObjName.getAttribute("value"), "Sirius");
+        sleep.sleeperAgent5();
+
+        WebElement radius = driver.findElement(By.cssSelector("#radius"));
+        radius.sendKeys("1.5");
+        Assert.assertEquals(radius.getAttribute("value"), "1.5");
+        sleep.sleeperAgent5();
+
+        WebElement searchButton = driver.findElement(By.cssSelector("body > div > div.query-container > div.search-input > div > form > input[type=submit]:nth-child(4)"));
+        searchButton.click();
+        sleep.sleeperAgent5();
+    }
+
+    /*@Test(priority = 6)
+    public void TicIDSearch() {
+        WebElement TicID = driver.findElement(By.xpath("//*[@id=\"search_input\"]"));
+        TicID.sendKeys("50559830");
+        Assert.assertEquals(TicID.getAttribute("value"), "50559830");
+        sleep.sleeperAgent3();
 
         WebElement radius = driver.findElement(By.xpath("//*[@id=\"radius\"]"));
         radius.sendKeys("178.6367");
-        sleep.sleeperAgent2();
+        Assert.assertEquals(radius.getAttribute("value"), "178.6367");
+        sleep.sleeperAgent3();
 
-        WebElement searchButton = driver.findElement(By.xpath("/html/body/div/div[2]/div[1]/div[1]/form/input[3]"));
+        WebElement searchButton = driver.findElement(By.cssSelector("body > div > div.query-container > div.search-input > div > form > input[type=submit]:nth-child(4)"));
         searchButton.click();
         sleep.sleeperAgent5();
+    }*/
 
-        //wait time for page to reload
-        Duration timeout = Duration.ofSeconds(10);
-        WebDriverWait wait = new WebDriverWait(driver, timeout);
-        WebElement element = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("/html/body/div/div[3]")));
-    }
-
-    public static void ObjNameSearch(WebDriver driver) {
-        WebElement RaDec = driver.findElement(By.id("search_input"));
-        RaDec.sendKeys("Sirius");
-        sleep.sleeperAgent1();
-
-        WebElement radius = driver.findElement(By.xpath("//*[@id=\"radius\"]"));
-        radius.sendKeys("40");
-        sleep.sleeperAgent2();
-
-        WebElement searchButton = driver.findElement(By.xpath("/html/body/div/div[2]/div[1]/div[1]/form/input[3]"));
-        searchButton.click();
-        sleep.sleeperAgent5();
-
-        //wait time for page to reload
-        Duration timeout = Duration.ofSeconds(10);
-        WebDriverWait wait = new WebDriverWait(driver, timeout);
-        WebElement element = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("/html/body/div/div[3]")));
-    }
-
-    public static void TicIDSearch(WebDriver driver) {
-        WebElement RaDec = driver.findElement(By.id("search_input"));
-        RaDec.sendKeys("50559830");
-        sleep.sleeperAgent1();
-
-        WebElement radius = driver.findElement(By.xpath("//*[@id=\"radius\"]"));
-        radius.sendKeys("178.6367");
-        sleep.sleeperAgent2();
-
-        WebElement searchButton = driver.findElement(By.xpath("/html/body/div/div[2]/div[1]/div[1]/form/input[3]"));
-        searchButton.click();
-        sleep.sleeperAgent5();
-
-        //wait time for page to reload
-        Duration timeout = Duration.ofSeconds(10);
-        WebDriverWait wait = new WebDriverWait(driver, timeout);
-        WebElement element = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("/html/body/div/div[3]")));
-    }
-
-    public static void CSV_upload(WebDriver driver){
-        WebElement fileinput = driver.findElement(By.name("csv_file"));
+    @Test(priority = 6)
+    public void CSV_upload() {
+        WebElement fileinput = driver.findElement(By.xpath("/html/body/div/div[2]/form/div/div/div[1]/input"));
         String filePath = "C:\\Users\\khuba\\Downloads\\data2.csv";
         fileinput.sendKeys(filePath);
-
-        WebElement CSVradius = driver.findElement(By.xpath("/html/body/div/div[2]/form/div/div/input[2]"));
-        CSVradius.sendKeys("20");
-        sleep.sleeperAgent3();
-
-        WebElement CSVsubmit = driver.findElement(By.xpath("/html/body/div/div[2]/form/div/div/input[3]"));
-        CSVsubmit.click();
-        sleep.sleeperAgent3();
-
     }
 
+    @Test(priority = 7)
+    public void CSV_Radius_Textbox(){
+        WebElement CSVradius = driver.findElement(By.xpath("/html/body/div/div[2]/form/div/div/div[2]/input[1]"));
+        CSVradius.sendKeys("0.5");
+        sleep.sleeperAgent3();
+    }
+
+    @Test(priority = 8)
+    public void CSV_Submit_Button(){
+        WebElement CSVsubmit = driver.findElement(By.xpath("/html/body/div/div[2]/form/div/div/div[2]/input[2]"));
+        CSVsubmit.click();
+        sleep.sleeperAgent3();
+    }
+
+
+
+    @AfterTest
+    public void teardown() {
+        // Quit the driver and close all associated windows
+        driver.quit();
+    }
 }
+
